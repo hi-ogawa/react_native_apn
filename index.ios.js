e2a726eee30441fb4c95741872493708eea1207d
hiogawa <hi.ogawa.zz@gmail.com>
2016-01-08 14:00:25 +0900

    copy react-native notification example from Facebook

--- index.ios.js
+++ index.ios.js
@@ -1,36 +1,199 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * The examples provided by Facebook are for non-commercial testing and
+ * evaluation purposes only.
+ *
+ * Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @flow
  */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
+  AlertIOS,
+  PushNotificationIOS,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
 } = React;
 
-var awesome_project = React.createClass({
+var Button = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TouchableHighlight
+      underlayColor={'white'}
+      style={styles.button}
+      onPress={this.props.onPress}>
+      <Text style={styles.buttonLabel}>
+      {this.props.label}
+      </Text>
+      </TouchableHighlight>
     );
   }
 });
 
+class NotificationExample extends React.Component {
+  componentWillMount() {
+    PushNotificationIOS.addEventListener('notification', this._onNotification);
+  }
+
+  componentWillUnmount() {
+    PushNotificationIOS.removeEventListener('notification', this._onNotification);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={styles.welcome}>
+      this is padding
+      this is padding
+      this is padding
+      </Text>
+      <Button
+      onPress={this._sendNotification}
+      label="Send fake notification"
+      />
+      </View>
+    );
+  }
+
+  _sendNotification() {
+    require('RCTDeviceEventEmitter').emit('remoteNotificationReceived', {
+      aps: {
+        alert: 'Sample notification',
+        badge: '+1',
+        sound: 'default',
+        category: 'REACT_NATIVE'
+      },
+    });
+  }
+
+  _onNotification(notification) {
+    AlertIOS.alert(
+      'Notification Received',
+      'Alert message: ' + notification.getMessage(),
+      [{
+        text: 'Dismiss',
+        onPress: null,
+      }]
+    );
+  }
+}
+
+class NotificationPermissionExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {permissions: null};
+  }
+
+  render() {
+    return (
+      <View>
+      <Button
+      onPress={this._showPermissions.bind(this)}
+      label="Show enabled permissions"
+      />
+      <Text>
+      {JSON.stringify(this.state.permissions)}
+      </Text>
+      </View>
+    );
+  }
+
+  _showPermissions() {
+    PushNotificationIOS.checkPermissions((permissions) => {
+      this.setState({permissions});
+    });
+  }
+}
+
+var styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonLabel: {
+    color: 'blue',
+  },
+});
+
+exports.title = 'PushNotificationIOS';
+exports.description = 'Apple PushNotification and badge value';
+exports.examples = [
+  {
+    title: 'Badge Number',
+    render(): React.Component {
+      PushNotificationIOS.requestPermissions();
+
+      return (
+        <View>
+        <Button
+        onPress={() => PushNotificationIOS.setApplicationIconBadgeNumber(42)}
+        label="Set app's icon badge to 42"
+        />
+        <Button
+        onPress={() => PushNotificationIOS.setApplicationIconBadgeNumber(0)}
+        label="Clear app's icon badge"
+        />
+        </View>
+      );
+    },
+  },
+  {
+    title: 'Push Notifications',
+    render(): React.Component {
+      return <NotificationExample />;
+    }
+  },
+  {
+    title: 'Notifications Permissions',
+    render(): React.Component {
+      return <NotificationPermissionExample />;
+    }
+  }];
+
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  */
+// 'use strict';
+//
+// var React = require('react-native');
+// var {
+//   StyleSheet,
+//   Text,
+//   View,
+// } = React;
+//
+// var awesome_project = React.createClass({
+//   render: function() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// });
+//
 var styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -50,4 +213,4 @@ var styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('awesome_project', () => awesome_project);
+AppRegistry.registerComponent('awesome_project', () => NotificationExample);
